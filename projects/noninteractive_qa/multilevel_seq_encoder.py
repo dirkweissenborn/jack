01a17e380816bd5d70629c9aa9d4b8c27014ab0e
Dirk Weissenborn <dirk.weissenborn@gmail.com>
2018-03-21 13:58:39 +0100

    fix

--- projects/noninteractive_qa/multilevel_seq_encoder.py
+++ projects/noninteractive_qa/multilevel_seq_encoder.py
@@ -109,18 +109,19 @@ def controller(sequence, length, controller_config, repr_dim, is_eval):
 
 
 def bow_segm_encoder(sequence, length, repr_dim, segm_ends, mask=None, normalize=False, transform=True):
-    seq_transformed = tf.layers.dense(sequence, repr_dim, tf.nn.relu) if transform else sequence
 
     segm_contributions = intra_segm_contributions(segm_ends, length)
     if mask is not None:
         segm_contributions *= tf.transpose(mask, [0, 2, 1])
-
-    segment_reps = tf.matmul(segm_contributions, seq_transformed)
     if normalize:
         bow_num = tf.matmul(segm_contributions, tf.ones_like(segm_ends))
-        segment_reps /= (bow_num + 1e-6)
+        sequence /= (bow_num + 1e-6)
 
-    return segment_reps
+    sequence = tf.matmul(segm_contributions, sequence)
+    seq_transformed = tf.layers.dense(sequence, repr_dim, tf.nn.relu,
+                                      name='bow_segm_dense') if transform else sequence
+
+    return seq_transformed
 
 
 def weighted_bow_segm_encoder(sequence, length, repr_dim, segm_ends, mask=None):
