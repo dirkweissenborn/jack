3bdfe5c568fbc5535ed503144d9b5c2905e687fc
Dirk <dirk.weissenborn@gmail.com>
2018-04-30 19:37:27 +0200

    hierarchical interactive segments model

--- projects/noninteractive_qa/model.py
+++ projects/noninteractive_qa/model.py
@@ -513,11 +513,11 @@ class HierarchicalSegmentInteractiveQAModule(AbstractXQAModelModule):
         # [B, S, Q]
         with tf.variable_scope('attention'):
             diag = tf.get_variable('attn_weight', [1, 1, encoded_question.get_shape()[-1].value], tf.float32,
-                                   initializer=tf.ones_initializer())
-            diag = tf.nn.relu(diag)
+                                   initializer=tf.zeros_initializer())
+            diag = tf.nn.sigmoid(diag)
             attn_scores = tf.einsum('abc,adc->abd', encoded_support, encoded_question * diag)
             attn_scores += tf.expand_dims(misc.mask_for_lengths(tensors.support_length), 2)
-            attn_scores /= math.sqrt(float(encoded_question.get_shape()[-1].value))
+            #attn_scores /= math.sqrt(float(encoded_question.get_shape()[-1].value))
             attn_prob = tf.nn.softmax(attn_scores, 1)
 
         question2support = tf.einsum('bsq,bqr->bsr', attn_prob, emb_question)
