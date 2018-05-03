5fa6327c3fdf8996061d29237ed06dec5ab9b288
Dirk Weissenborn <dirk.weissenborn@gmail.com>
2018-05-03 16:10:07 +0200

    hierarchical selfattn model

--- projects/noninteractive_qa/multilevel_seq_encoder.py
+++ projects/noninteractive_qa/multilevel_seq_encoder.py
@@ -364,7 +364,7 @@ def segment_self_attention(ctrl, seq, length, is_eval, key_dim, scaled=True, key
     attn_probs = tf.nn.softmax(tf.concat([s, attn_scores], 2), 2)
     attn_probs = attn_probs[:, :, 1:]
 
-    attn_states = tf.einsum('abdh,adhc->abhc', attn_probs, seq)
+    attn_states = tf.einsum('abdh,adc->abhc', attn_probs, seq)
 
     return attn_scores, attn_probs, attn_states, edge_probs, edge_logits
 
